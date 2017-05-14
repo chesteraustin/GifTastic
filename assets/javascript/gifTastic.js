cae7b8de6c975bc4f96f5331be4804b32f95b866
chesteraustin <chesteraustin@gmail.com>
2017-05-14 12:09:08 -0700

    dipslay TOPICS buttons and ADD function

--- assets/javascript/gifTastic.js
+++ assets/javascript/gifTastic.js
@@ -1,3 +1,47 @@
 $(document).ready(function(){
-	console.log("ready")
+	var topics = [
+		"red panda",
+		"bunnies",
+		"ducks",
+		"cows"
+	]
+
+	//display buttons
+	displayButtons(topics);
+
+	//Create listener to add topic
+	$("#addButton").on("click", function(){
+		var newTopic = $("#addTopic").val();
+		topics.push(newTopic)
+		displayButtons(topics);
+	})
+
+	//Create listener for topic
+	$("#topicsContainer").on("click", ".topic-button", function(){
+		var topicSelected = $(this).attr("data-topic");
+		showGifs(topicSelected)
+	})
+
 })
+
+function showGifs(topic) {
+
+}
+
+function displayButtons(topics) {
+	console.log(topics)
+	//Clear container
+	$("#topicsContainer").children().not(".add-topic").remove();
+
+	for (var i = 0; i < topics.length; i++) {
+		var topic = topics[i];
+		//Create button
+		newButton = $("<button>");
+		newButton.text(topic);
+		newButton.addClass("topic-button");
+		newButton.attr("data-topic", topic)
+
+		//Append to 
+		$("#topicsContainer").append(newButton);
+	}
+}
